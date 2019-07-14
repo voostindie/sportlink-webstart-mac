83cf751c358e0559ae11c7e689954cc35620e498
Vincent Oostindie <vincent@ulso.nl>
2019-07-14 17:28:33 +0200

    Betere formattering van het script

--- sportlink.sh
+++ sportlink.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 DIR=`dirname $0`
-/usr/libexec/java_home -v 1.8 --exec java -cp $DIR/javaws.jar net.sourceforge.jnlp.runtime.Boot -Xnofork -jnlp https://club.sportlink.com/apps/club-production/nevobo.jnlp
+/usr/libexec/java_home -v 1.8 \
+    --exec java -cp $DIR/javaws.jar \
+    net.sourceforge.jnlp.runtime.Boot \
+    -Xnofork \
+    -jnlp https://club.sportlink.com/apps/club-production/nevobo.jnlp
