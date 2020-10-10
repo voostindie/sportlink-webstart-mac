ba45d4293ae858464a81e22feb26ac8b0b81af23
Vincent Oostindie <vincent@ulso.nl>
2020-10-10 11:47:44 +0200

    Fix: betere selectie van de te gebruiken JVM

--- sportlink.sh
+++ sportlink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIR=`dirname $0`
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk use java `sdk ls java | grep "8.*.j9-adpt" | cut -f 6 -d "|"`
+sdk use java `sdk ls java | grep "8.*.j9-adpt" | grep "local only" | cut -f 6 -d "|"`
 java -cp $DIR/javaws.jar \
     net.sourceforge.jnlp.runtime.Boot \
     -Xnofork \
@@ -12,3 +12,7 @@ java -cp $DIR/javaws.jar \
 # 50 MB bij. Idealiter zorg ik dat dat wel werkt, maar zolang dat niet
 # het geval is kunnen we cache beter helemaal opruimen:
 rm -rf ~/.cache/icedtea-web/cache
+
+# De J9 Java runtime bouwt een cache van classes voor het geval de JVM
+# wordt hergebruikt over meerdere applicaties tegelijk.
+rm -rf ~/javasharedresources
